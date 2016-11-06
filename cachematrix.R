641e1bdb1ecc8b55b0035dc1d0376defaf7a41c6
carlosrf49 <carlosfrancisco.rodriguezf@gmail.com>
2016-11-06 16:47:51 -0600

    Revert "AssigmentWeek3"
    This reverts commit 089758d3a32361a884708d99508e4a2ec267c49f.

--- cachematrix.R
+++ cachematrix.R
@@ -3,38 +3,13 @@
 
 ## Write a short comment describing this function
 
-  makeCacheMatrix <- function(x = matrix()) {
-  require(MASS)  
-    
-    m <- NULL
-  
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(ginv) m <<- ginv
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-  
-  }
-  
-  
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  require(MASS)  
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- ginv(data, ...)
-  x$setinverse(m)
-  m
 }
